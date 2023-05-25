2428fa6979203e0e928a89550242ade304f76bdc
linecomment <3062876335@qq.com>
2023-05-25 23:10:30 +0800

    update

---
+++ src/components/FormPop.vue
@@ -0,0 +1,101 @@
+﻿<template>
+  <div class="line-pop">
+    <div class="form">
+      <van-field
+        :name="dataLabel.label1"
+        :label="dataLabel.label1"
+        :placeholder="dataLabel.label1"
+        readonly
+      />
+      <van-field
+        v-model="updateValue"
+        :name="dataLabel.label2"
+        :label="dataLabel.label2"
+        placeholder="请填写"
+      />
+    </div>
+
+    <div class="button">
+      <van-button plain type="success" @click="cancle">取消</van-button>
+      <van-button plain type="warning" @click="save"> 修改 </van-button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref, defineProps, reactive, onMounted } from "vue";
+// const formData = reactive({
+//     formData
+// })
+const dataLabel = reactive({
+  label1: null,
+  label2: null,
+});
+const updateValue = ref(null);
+const props = defineProps({
+  formData: Object,
+  activeIndex: String,
+});
+onMounted(() => {
+  const i = props.activeIndex;
+  if (i === 0) {
+    // 头像
+    dataLabel.label1 = "原头像";
+    dataLabel.label2 = "新头像";
+  } else if (i === 1) {
+    // 昵称
+    dataLabel.label1 = "原昵称";
+    dataLabel.label2 = "新昵称";
+  } else if (i === 2) {
+    // 密码
+    dataLabel.label1 = "原密码";
+    dataLabel.label2 = "新密码";
+  } else if (i === 3) {
+    // 手机号
+    dataLabel.label1 = "原手机号";
+    dataLabel.label2 = "新手机号";
+  } else if (i === 4) {
+    // 生日
+    dataLabel.label1 = "原生日";
+    dataLabel.label2 = "新生日";
+  } else {
+    // 邮箱
+    dataLabel.label1 = "原邮箱";
+    dataLabel.label2 = "新邮箱";
+  }
+});
+
+const save = () => {
+  const i = props.activeIndex;
+  if (i === 0) {
+    // 头像
+  } else if (i === 1) {
+    // 昵称
+  } else if (i === 2) {
+    // 密码
+  } else if (i === 3) {
+    // 手机号
+  } else if (i === 4) {
+    // 生日
+  } else {
+    // 邮箱
+  }
+  // 发送post请求
+};
+</script>
+
+<style lang="less" scoped>
+.line-pop {
+  height: 100%;
+  .form {
+    margin-top: 1rem;
+    color: red !important;
+  }
+  .button {
+    margin-top: 1rem;
+    margin-bottom: 1rem;
+    justify-content: space-around;
+    display: flex;
+  }
+}
+</style>
--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,117 +1,184 @@
 ﻿<template>
   <div class="line-profile">
     <van-form>
-      <van-field
-      v-model="formData.email"
-      name="email"
-      center
-      clearable
-      label="短信验证码"
-      placeholder="请输入短信验证码"
-    >
-      <template #button>
-        <van-icon @click="edit($event)" name="edit" size="larger"></van-icon>
-      </template>
-    </van-field>
       <van-cell-group inset>
         <van-field
           v-model="formData.avatar"
           name="avatar"
           label="头像"
           placeholder="未上传"
-          readonly="status"
+          readonly
+          ref="fieldname"
+        >
+          <template #button>
+            <van-icon
+              @click="edit(0)"
+              name="add-o"
+              size="larger"
+            ></van-icon> </template
         ></van-field>
         <van-field
           v-model="formData.nickname"
           name="nickname"
           label="昵称"
           placeholder="未填写"
-          readonly="status"
+          readonly
           :rules="[{ required: true, message: '请填写昵称' }]"
+          ref="fieldname"
+        >
+          <template #button>
+            <van-icon
+              @click="edit(1)"
+              name="edit"
+              size="larger"
+            ></van-icon> </template
         ></van-field>
         <van-field
-          v-model="formData.username"
-          name="username"
-          label="用户名"
+          v-model="formData.userId"
+          name="userId"
+          label="用户 ID"
           placeholder=""
           readonly
-        ></van-field>
+        >
+        </van-field>
         <van-field
           v-model="formData.password"
           type="password"
           name="password"
           label="密码"
           placeholder="密码"
-          readonly="status"
-        ></van-field>
+          readonly
+        >
+          <template #button>
+            <van-icon @click="edit(2)" name="edit" size="larger"></van-icon>
+          </template>
+        </van-field>
         <van-field
           v-model="formData.phone"
           name="phone"
           label="手机号"
           placeholder="未填写"
           :rules="[{ required: true, message: '请填写手机号' }]"
-          readonly="status"
+          readonly
+        >
+          <template #button>
+            <van-icon
+              @click="edit(3)"
+              name="edit"
+              size="larger"
+            ></van-icon> </template
         ></van-field>
         <van-field
           v-model="formData.birthday"
           name="birthday"
           label="生日"
           placeholder="未设置"
-          readonly="status"
+          readonly
           :rules="[{ required: true, message: '请填写出生日期' }]"
-        ></van-field
-        ><van-field
+        >
+          <template #button>
+            <van-icon
+              @click="edit(4)"
+              name="edit"
+              size="larger"
+            ></van-icon> </template
+        ></van-field>
+        <van-field
           v-model="formData.email"
           name="email"
           label="邮箱"
           placeholder="还未设置"
           :rules="[{ required: true, message: '请填写邮箱' }]"
-          readonly="status"
+          readonly
+        >
+          <template #button>
+            <van-icon
+              @click="edit(5)"
+              name="edit"
+              size="larger"
+            ></van-icon> </template
         ></van-field>
       </van-cell-group>
-      <div style="margin: 16px">
-        <van-button round block type="primary"  @click="editOrSave">
-          {{ status ? '修改' : '保存' }}
-        </van-button>
-      </div>
     </van-form>
     <div class="logut">
-      <van-button @click="logout" style="color:rgba(0, 0, 0, 0.633)"  plain block>退出登入</van-button>
+      <van-button
+        @click="logout"
+        style="color: rgba(0, 0, 0, 0.633)"
+        plain
+        block
+        >退出登入</van-button
+      >
     </div>
   </div>
+  <van-popup v-model:show="showPop">
+    <div v-if="activeIndex === 0">
+      <form-pop :formData="formData" :activeIndex="activeIndex"></form-pop>
+    </div>
+    <div v-else-if="activeIndex === 1">
+      <van-field
+        name="nickname"
+        label="原昵称"
+        :placeholder="formData.nickname"
+        readonly
+      ></van-field>
+      <van-field
+        v-model="updateForm.nickname"
+        name="nickname"
+        label="新昵称"
+        placeholder="未填写"
+        :rules="[{ required: true, message: '请填写昵称' }]"
+      ></van-field>
+    </div>
+    <div v-else-if="activeIndex === 2">
+      <form-pop :formData="formData" :activeIndex="activeIndex"></form-pop>
+    </div>
+    <div v-else-if="activeIndex === 3">
+      <form-pop :formData="formData" :activeIndex="activeIndex"></form-pop>
+    </div>
+    <div v-else-if="activeIndex === 4">
+      <form-pop :formData="formData" :activeIndex="activeIndex"></form-pop>
+    </div>
+    <div v-else-if="activeIndex === 5">
+      <form-pop :formData="formData" :activeIndex="activeIndex"></form-pop>
+    </div>
+  </van-popup>
 </template>
 
 <script setup>
+import FormPop from "@/components/FormPop.vue";
 import { ref, reactive } from "vue";
-let status = ref('false')
+const showPop = ref(true);
+const fieldname = ref(null);
+const activeIndex = ref(0);
+
 const formData = reactive({
-  avatar: '',
-  nickname: '',
-  username: '121321 ',
-  password: '',
-  birthday: '',
-  phone: '',
-  email:''
+  avatar: "",
+  nickname: "linecomment",
+  userId: "121321 ",
+  password: "1323456",
+  birthday: "2003-09-21",
+  phone: "17709891231",
+  email: "3212012901@qq.com",
 });
 
-const editOrSave = ()=>{
-  if(status.value){
-    // 修改
-  } else {
-    // 保存
-  }
-  status.value = !status.value
-}
-const logout = ()=>{
+const updateForm = reactive({
+  avatar: "",
+  nickname: "",
+  password: "",
+  birthday: "",
+  phone: "",
+  email: "",
+});
+
+const logout = () => {
   // 登出
-  console.log('logout')
-}
-const edit = ($event) => {
-  console.log($event.target)
-}
-
-
+  console.log("logout");
+};
+const edit = (i) => {
+  showPop.value = true;
+  activeIndex.value = i;
+};
 </script>
 
-<style>
+<style lang="less" scoped>
 </style>
